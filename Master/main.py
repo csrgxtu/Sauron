bffa9231591108517d371278ead646f1c77e1713
archer <1246506786@qq.com>
2015-11-16 03:32:15 -0500

    flask-restful is amazing

--- Master/main.py
+++ Master/main.py
@@ -8,12 +8,16 @@
 #
 # Produced By BR
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-if __name__ == "__main__":
-    app.run()
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
