8ea853182a47d1e5c338c842fc114ef5a7b0cf29
archer <1246506786@qq.com>
2015-11-17 20:50:38 -0500

    file mix mode support ok

--- Master/FileRes.py
+++ Master/FileRes.py
@@ -17,23 +17,25 @@ from MongoHelper import MongoHelper
 class FileRes(Resource):
     def put(self):
         # wanting {'files': [file]}
-        # file: {'url': url, 'head': 'http head', 'body': 'html'}
+        # file: {'url': url, 'head': 'http head', 'body': 'html', 'spider': 'google'}
         Json = request.get_json(force=True)
         documents = []
 
         for doc in Json['files']:
             u = uuid.uuid4()
-            documents.append({'url': doc['url'], 'filename': u.hex + '.html'})
+            documents.append({'url': doc['url'], 'filename': u.hex + '.html', 'spider': doc['spider']})
             self.save(doc['head'] + '\n\n' + doc['body'], u.hex + '.html')
 
         MH = MongoHelper('localhost', 27017)
         UrlReturn['data'] = MH.insertFile({"documents": documents})
+        MH.close()
         return UrlReturn
 
     def get(self):
         args = request.args
         MH = MongoHelper('localhost', 27017)
-        documents = MH.readFile(args['start'], args['offset'])
+        documents = MH.readFile(args['spider'], args['start'], args['offset'])
+        MH.close()
         UrlReturn['data'] = documents
         return UrlReturn
 
