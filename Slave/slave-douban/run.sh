6fd173585148d3e7996426c163089727c0bb22d7
JohnTian <tianzhaixing@126.com>
2015-11-20 12:37:21 +0800

    remove -s JOBDIR setting in run.sh.

--- Slave/slave-douban/run.sh
+++ Slave/slave-douban/run.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-    scrapy crawl douban -a url='http://192.168.100.3:5000/unvisitedurls?start=0&offset=50&spider=douban' -s JOBDIR=crawls/douban
+    scrapy crawl douban -a url='http://192.168.100.3:5000/unvisitedurls?start=0&offset=10&spider=douban'
 done
