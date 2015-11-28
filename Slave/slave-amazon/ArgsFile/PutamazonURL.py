61bee2b2ae6c06ae744bdb9f905913d54cc7d757
JohnTian <tianzhaixing@126.com>
2015-11-28 19:13:52 +0800

    put amazon URL to master machine.

---
+++ Slave/slave-amazon/ArgsFile/PutamazonURL.py
@@ -0,0 +1,19 @@
+import unirest
+import json
+
+if (__name__=='__main__'):
+
+    #!< amazonURL.json
+    with open('./amazonURL.json', 'rb') as f5:
+        uvdict = json.load(f5)
+    f5.close()
+
+    uvlist = uvdict['urls']
+    #print len(uvlist), uvlist[0]
+
+    unirest.timeout(180)
+    resunvisitedurl = unirest.put(
+                    "http://192.168.100.3:5000/unvisitedurls",
+                    headers={ "Accept": "application/json", "Content-Type": "application/json" },
+                    params=json.dumps(uvdict)
+                )
